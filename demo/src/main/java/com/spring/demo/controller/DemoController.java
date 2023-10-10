668f4fbd32653b05b52cccd98e744514f6b81d6a
museeskinder <eskindermuse@gmail.com>
2023-10-10 20:17:32 +0300

    adding attribute: userName

--- demo/src/main/java/com/spring/demo/controller/DemoController.java
+++ demo/src/main/java/com/spring/demo/controller/DemoController.java
@@ -1,12 +1,14 @@
 package com.spring.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DemoController {
     @RequestMapping(value = {"/home", "/index"})
-    public String demoControllerMethod() {
+    public String demoControllerMethod(Model model) {
+        model.addAttribute("userName", "Muse");
         return "index.html";
     }
 }
